b8f4b12a83a33aa3459f5fa71251e127a19a8852
Neerajkumar151 <thakurneerajkumar17@gmail.com>
2026-01-23 12:07:08 +0530

    update

--- components/WorkSlider.jsx
+++ components/WorkSlider.jsx
@@ -14,9 +14,9 @@ const workSlides = {
     {
       images: [
         { title: "Krishisanjivni", path: "/krishisanjivni.png", link: "https://krishisanjivni.vercel.app/" },
+        { title: "LoanPal", path: "/ez.png", link: "https://loan-pal-new.vercel.app/" },
         { title: "GenieAi", path: "/genieai.png", link: "https://genie-ai-wine.vercel.app/" },
         { title: "SkyDrive", path: "/skydrive.PNG", link: "https://cloud-storage-webapp.vercel.app/sign-in" },
-        { title: "EY hackathon Project", path: "/ez.png", link: "https://loan-pal-new.vercel.app/" },
         { title: "3 sem mini Project", path: "/3 sem project.PNG", link: "https://neerajkumar151.github.io/Mini-Project/" },
         { title: "Amazon Clone", path: "/amazon.PNG", link: "https://neerajkumar151.github.io/Amazon-frontend/clone_amazon.html" },
         
